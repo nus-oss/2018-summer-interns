7dcbba84871203f4c26d28843aceb65d4578a6bc
Damith C. Rajapakse <damith@gmail.com>
2018-07-27 10:30:35 +0800

    at 26-07-2018 (v1.1.2)

---
+++ run.sh
@@ -0,0 +1,2 @@
+java -jar RepoSense.jar -config interns.csv -output report$1/ -since 19/05/2018 -formats java adoc js md css jsp tag xml fxml gradle
+java -jar RepoSense.jar -view report$1/reposense-report
